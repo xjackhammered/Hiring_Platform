13d7399bbde4194021ff1d7e53f5ff60223cf4a7
xJackhammered <hammerofjack7@gmail.com>
2026-01-29 12:50:27 +0600

    added the job model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(max_length=30, null=False, blank=False)
+    description = models.TextField(max_length=200, blank=False, null=False)
+    archived = models.BooleanField(default=False)
+    posted_at = models.DateTimeField(auto_now_add=True)
